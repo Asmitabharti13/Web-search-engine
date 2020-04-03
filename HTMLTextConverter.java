8ce613c3e4f21e7208c5e2e78e0170f8e3e4808f
Asmitabharti13 <61602236+Asmitabharti13@users.noreply.github.com>
2020-04-03 17:06:41 -0400

    Add files via upload

---
+++ HTMLTextConverter.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+import org.jsoup.Jsoup;
+
+public class HTMLTextConverter 
+{
+	/**
+	 * This method converts HTML files into text files.
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 * @throws NullPointerException
+	 */
+	public static void convertHtmlToText() 
+			throws IOException, FileNotFoundException, NullPointerException
+	{
+		org.jsoup.nodes.Document myDocument = null;
+		BufferedWriter bufferedWriter = null;
+
+		try
+		{
+			File dir = new File("C:\\Users\\Asmita\\eclipse-workspace\\MyWebSearchEngine\\src\\HTMLFiles\\");
+			File[] files = dir.listFiles();
+			int i = 0;
+			for (File file : files) 
+			{
+				myDocument = Jsoup.parse(file, "UTF-8");
+				String myString = file.getName().substring(0, file.getName().lastIndexOf('.'));
+				bufferedWriter = new BufferedWriter(new FileWriter(
+						"C:\\Users\\Asmita\\eclipse-workspace\\MyWebSearchEngine\\ConvertedTextFiles\\" + myString + ".txt"));
+				bufferedWriter.write(myDocument.text());
+				bufferedWriter.close();
+			}
+		} 
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
